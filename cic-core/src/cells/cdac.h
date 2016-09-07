af0a199302dbd61f295d918bbfe0ed2ed0882d1c
Carsten Wulff <carsten@wulff.no>
2016-09-07 20:34:25 +0200

    Changed to GPL license

---
+++ cic-core/src/cells/cdac.h
@@ -0,0 +1,45 @@
+//====================================================================
+//        Copyright (c) 2016 Carsten Wulff Software, Norway 
+// ===================================================================
+// Created       : wulff at 2016-9-4
+// ===================================================================
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+// 
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+// 
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//====================================================================
+
+
+#ifndef CIC_CELLS_CDAC_H
+#define CIC_CELLS_CDAC_H
+
+#include "core/layoutcell.h"
+
+namespace cIcCells{
+
+    class CDAC: public cIcCore::LayoutCell
+    {
+        Q_OBJECT
+
+    public:
+        virtual void place();
+        virtual void route();
+        
+    private:
+        
+
+    };
+
+
+};
+Q_DECLARE_METATYPE(cIcCells::CDAC)
+
+#endif 
