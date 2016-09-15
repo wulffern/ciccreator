7d00e2d0a569de2db1477501fa1a4bd47bc8d0b2
Carsten Wulff <carsten@wulff.no>
2016-09-15 17:47:03 +0200

    Port can't mirror, since it follows the rect, but InstancePort must mirror, since it's not connected to the cell rectangle

--- cic-core/src/core/instanceport.cpp
+++ cic-core/src/core/instanceport.cpp
@@ -2,35 +2,46 @@
 
 namespace cIcCore{
 
-InstancePort::InstancePort(QString name, Port * p, Rect * parent):Port(name)
-{
-  childport_ = p;
-  Rect *r = p->get();
-  if(r){
-    Layer * l = rules->getLayer(r->layer());
-    routeLayer_ = l;
-    this->setRect(r->layer(), r->x1(), r->y1(),r->width(),r->height());
 
-    }
-}
-
-
-
-
-InstancePort::~InstancePort()
-{
-
-}
-
-
-QString InstancePort::childName(){
-  if(childport_){
-      return childport_->name();
-    }else{
-      return QString("");
+    void InstancePort::mirrorX(int ay)
+    {
+        Rect::mirrorX(ay);
     }
 
-}
+    void InstancePort::mirrorY(int ax)
+    {
+        Rect::mirrorY(ax);
+    }
+
+    InstancePort::InstancePort(QString name, Port * p, Rect * parent):Port(name)
+    {
+        childport_ = p;
+        Rect *r = p->get();
+        if(r){
+            Layer * l = rules->getLayer(r->layer());
+            routeLayer_ = l;
+            this->setRect(r->layer(), r->x1(), r->y1(),r->width(),r->height());
+
+        }
+    }
+
+
+
+
+    InstancePort::~InstancePort()
+    {
+
+    }
+
+
+    QString InstancePort::childName(){
+        if(childport_){
+            return childport_->name();
+        }else{
+            return QString("");
+        }
+
+    }
 
 
 
